3263b529e761c88c5281572ab8b6160e8ac51993
SamerSiam <ssiam05@gmail.com>
2021-11-14 09:37:28 +0200

    print the word

--- 5.8_longest_word.js
+++ 5.8_longest_word.js
@@ -1,6 +1,7 @@
 const longestWords=(str)=>
 {
     let strArr= str.split(' ');
+    let returnStr='';
     console.log(strArr);
     let longestWord=0;
     for (let i=0; i<strArr.length; i++)
@@ -8,10 +9,12 @@ const longestWords=(str)=>
           if (strArr[i].length > longestWord)
           {
             longestWord=strArr[i].length;
+            returnStr=strArr[i];
           }
     }
    
-    return longestWord;
+    return returnStr;
 }
 const sampleArr= "school egg kite me necessary beautiful";
-console.log (longestWords(sampleArr));
+console.log (`Sample array : ${sampleArr}`);
+console.log ("Longest word is: " +longestWords(sampleArr));
